18a1de1ac8e139b2c1efd6f332fd2d79f3610e19
setyarp <setyarizkypradana@gmail.com>
2023-12-05 16:24:10 +0700

    update: controllers

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,59 +1,7 @@
 const { Admin } = require("../models");
-const { JWT_SECRET } = process.env;
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 module.exports = {
-  login: async (req, res) => {
-    try {
-      const { username, password } = req.body;
-
-      const admin = await Admin.findOne({
-        where: {
-          username,
-        },
-      });
-
-      if (!admin) {
-        return res.status(404).json({
-          status: "failed",
-          message: "Username tidak ada",
-        });
-      }
-
-      const isValidPassword = await bcrypt.compare(password, admin.password);
-
-      if (!isValidPassword) {
-        return res.status(404).json({
-          status: "failed",
-          message: "Password salah",
-        });
-      }
-
-      const payload = {
-        id: admin.id,
-        username: admin.username,
-        role: admin.role,
-      };
-
-      const token = jwt.sign(payload, JWT_SECRET);
-
-      const data = {
-        username: admin.username,
-        role: admin.role,
-      };
-
-      return res.status(200).send({
-        status: true,
-        message: `Selamat datang ${admin.username}`,
-        data,
-        token,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
   create: async (req, res) => {
     try {
       const { username, password } = req.body;
