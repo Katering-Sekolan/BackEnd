c5d6f69adeeef8bc2e96b8c4c04bd036eed5d391
setyarp <setyarizkypradana@gmail.com>
2023-12-15 17:11:38 +0700

    Feat: wa not clean

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -1,19 +1,22 @@
-const client = require("../app");
-const { User } = require("../models");
+// const client = require("../app");
+// const { User } = require("../models");
+// const io = require("../app");
 
-module.exports = {
-  sendMessage: async (req, res) => {
-    try {
-      const { number, message } = req.body;
-      const formattedPhone = number + "@c.us";
-      client.sendMessage(formattedPhone, message);
+// module.exports = {
+// 	sendMessage: async (req, res) => {
+// 		const { number, message } = req.body;
+// 		const formattedPhone = number + "@c.us";
+// 		console.log(formattedPhone);
+// 		console.log(message);
+// 		try {
+// 			await client.sendMessage(formattedPhone, message);
 
-      return res.status(200).json({
-        status: "success",
-        message: `Berhasil mengirim pesan ke nomor hp ${formattedPhone}`,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-};
+// 			return res.status(200).json({
+// 				status: "success",
+// 				message: `Berhasil mengirim pesan ke nomor hp ${formattedPhone}`,
+// 			});
+// 		} catch (err) {
+// 			console.log(err);
+// 		}
+// 	},
+// };
