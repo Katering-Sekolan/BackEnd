60decfc9aef6b1bddd2aac0f7589ba1f578ca418
wahyuwardana2 <wahyu.wardana100@gmail.com>
2024-01-21 15:06:36 +0700

    ok

--- controllers/countController.js
+++ controllers/countController.js
@@ -1,5 +1,5 @@
-const { where } = require("sequelize");
-const { User, Admin, TagihanBulanan } = require("../models");
+const { Sequelize, Op } = require("sequelize");
+const { User, Admin, TagihanBulanan, Pembayaran } = require("../models");
 
 module.exports = {
   countUser: async (req, res) => {
@@ -29,23 +29,65 @@ module.exports = {
     }
   },
   countBill: async (req, res) => {
-    const { month } = req.params;
-
     try {
-      const bill = await TagihanBulanan.count(
-        where(
-          Sequelize.fn("DATE_FORMAT", Sequelize.col("bulan"), "%Y-%m"),
-          month
-        )
-      );
+
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentYear = currentDate.getFullYear();
+
+      const totalTagihan = await TagihanBulanan.sum("total_tagihan", {
+        where: Sequelize.and(
+          Sequelize.fn("MONTH", Sequelize.col("bulan")),
+          currentMonth
+        ),
+      });
 
       return res.status(200).json({
         status: "success",
-        message: "Berhasil mendapatkan jumlah tagihan",
-        data: bill,
+        message: `Total tagihan bulan ini (${currentMonth}-${currentYear}): ${totalTagihan}`,
+        data: totalTagihan,
       });
     } catch (err) {
       console.log(err);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error.",
+      });
+    }
+  },
+  countLunas: async (req, res) => {
+    try {
+      const currentDate = new Date();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentYear = currentDate.getFullYear();
+
+      const countLunasPayments = await Pembayaran.count({
+        where: {
+          [Op.and]: [
+            Sequelize.where(
+              Sequelize.fn("MONTH", Sequelize.col("tanggal_pembayaran")),
+              currentMonth
+            ),
+            Sequelize.where(
+              Sequelize.fn("YEAR", Sequelize.col("tanggal_pembayaran")),
+              currentYear
+            ),
+            { status_pembayaran: "LUNAS" },
+          ],
+        },
+      });
+
+      return res.status(200).json({
+        status: "success",
+        message: `Total number of payments with status 'LUNAS' for the current month (${currentMonth}-${currentYear}): ${countLunasPayments}`,
+        data: countLunasPayments,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal server error.",
+      });
     }
   },
 };
